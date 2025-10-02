942c1392669962272da4ef0b4453bf942f63d9ba
root <root@XT.>
2025-10-02 18:35:57 -0400

    updated setup.py to include cluster_cpp as extension

--- my_rosbag_reader/setup.py
+++ my_rosbag_reader/setup.py
@@ -1,6 +1,22 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup, Extension
+import pybind11
+import os
 
 package_name = 'my_rosbag_reader'
+
+ext_modules = [
+    Extension(
+        "cluster_cpp",
+        ["cluster.cpp"],
+        include_dirs=[
+            pybind11.get_include(), 
+            pybind11.get_include(user=True),
+            "/usr/include/eigen3",
+        ],
+        language="c++",
+        extra_compile_args=["-O3", "-std=c++17"],
+    )
+]
 
 setup(
     name=package_name,
@@ -11,6 +27,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
+    ext_modules=ext_modules,
     install_requires=['setuptools', 'rclpy', 'std_msgs', 'cev_msgs'],
     zip_safe=True,
     entry_points={
