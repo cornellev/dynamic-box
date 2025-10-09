5c5d4abd16bae3da5b1fdf96b21b4eece1c4e419
Lucas Libshutz <lsl94@cornell.edu>
2025-10-09 01:04:57 -0400

    fixed `setup.py` file, also removed test cloud
    that should be moved to own branch to avoid commit/push delay, as it is
    a really large file

--- my_rosbag_reader/setup.py
+++ my_rosbag_reader/setup.py
@@ -1,16 +1,15 @@
-<<<<<<< HEAD
 from setuptools import find_packages, setup, Extension
 import pybind11
 import os
 
-package_name = 'my_rosbag_reader'
+package_name = "my_rosbag_reader"
 
 ext_modules = [
     Extension(
-        "cluster_cpp",
+        "cluster.cpp",
         ["cluster.cpp"],
         include_dirs=[
-            pybind11.get_include(), 
+            pybind11.get_include(),
             pybind11.get_include(user=True),
             "/usr/include/eigen3",
         ],
@@ -19,31 +18,21 @@ ext_modules = [
     )
 ]
 
-=======
-from setuptools import find_packages, setup
-
-package_name = 'my_rosbag_reader'
-
->>>>>>> 73852507092b44cc445d3e1af79b72b3fa076d0f
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-<<<<<<< HEAD
     ext_modules=ext_modules,
-=======
->>>>>>> 73852507092b44cc445d3e1af79b72b3fa076d0f
-    install_requires=['setuptools', 'rclpy', 'std_msgs', 'cev_msgs'],
+    install_requires=["setuptools", "rclpy", "std_msgs", "cev_msgs"],
     zip_safe=True,
     entry_points={
-        'console_scripts': [
-            'listener = my_rosbag_reader.live:main',
-            'rs_listener = my_rosbag_reader.surface:main'
+        "console_scripts": [
+            "listener = my_rosbag_reader.live:main",
+            "rs_listener = my_rosbag_reader.surface:main",
         ],
     },
 )
